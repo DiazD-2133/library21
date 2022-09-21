9a34cfe66480e82c528142deff47a1b0ffd794c7
Deiker Diaz <deiker21diaz@gmail.com>
2022-09-20 21:57:25 -0400

    add django-environ

--- config/settings.py
+++ config/settings.py
@@ -13,17 +13,22 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import sys
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 sys.path.append(str(BASE_DIR / 'modules'))
 
+# config/ .env Secret keys
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%$v*hm2*$nakpg0w%n$*^^-+b@6r9%5=!t+&tet+l7_*sqb-ck'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
